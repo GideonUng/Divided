345b17fc09d11d34bb428eda52f9429b251002d3
GideonUng <gidcheen@gmail.com>
2015-03-10 21:52:24 +0100

    del unused files / added platfrom moving script

---
+++ Assets/MovePlarform.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlarform : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        }
+    }
+}
