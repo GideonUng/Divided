d08aab9de5e544066cb5d7de3c80dc12f93aeb6d
GideonUng <gidcheen@gmail.com>
2015-03-14 14:07:11 +0100

    added restart button

---
+++ Assets/Restart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Restarter
+{
+	public static void RestartLevel()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
---
+++ Assets/RestartButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartButton : MonoBehaviour
+{
+	public void Restart()
+	{
+		Restarter.RestartLevel();
+	}
+}
