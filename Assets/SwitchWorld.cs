d373d8cf767d257b948eccbcd61fd325e2533975
GideonUng <gidcheen@gmail.com>
2015-03-31 22:50:45 +0200

    added switch world

---
+++ Assets/SwitchWorld.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchWorld : MonoBehaviour
+{
+	public Camera CameraWhite;
+	public Camera CameraBlack;
+
+	void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			if (CameraBlack.depth == 1)
+			{
+				CameraBlack.depth = 0;
+				CameraWhite.depth = 1;
+			}
+			else if (CameraWhite.depth == 1)
+			{
+				CameraBlack.depth = 1;
+				CameraWhite.depth = 0;
+			}
+		}
+	}
+}
