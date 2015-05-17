a5aaa498cebdb8eefa36b5ce4365838cb6863d46
Robert Tieber <robert.tieber@gmail.com>
2015-05-17 21:05:54 +0200

    Popup init script

---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Popup : MonoBehaviour
+{
+
+	public GameObject popupPanel;
+
+	public string text;
+	public string actionName;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		GameObject popups = GameObject.Find ("Popups");
+		GameObject popup = Instantiate (popupPanel) as GameObject;
+		popup.transform.SetParent (popups.transform, false);
+		SetText ("ActionText", actionName.ToUpper ());
+		SetText ("QuestionText", text);
+	}
+	
+	void SetText (string textName, string text)
+	{
+		GameObject textGameObject = GameObject.Find (textName);
+		Text textComponent = textGameObject.GetComponent<Text> ();
+		textComponent.text = text;
+	}
+}
